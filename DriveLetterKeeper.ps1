56900c9e2b4e3246c993b9b1b56ea2cd5d603a44
Allison <acreely@outlook.com>
2017-09-14 11:01:50 -0700

    Style improvements
    Removed unnecessary "echo" commands.

--- DriveLetterKeeper.ps1
+++ DriveLetterKeeper.ps1
@@ -12,7 +12,7 @@ function Set-DriveLetter($DriveLabel,$DriveLetter) {
 	# Check to make sure there isn't already a disk mounted to the desired drive letter.
 	$check1 = Get-WmiObject -Class win32_volume -Filter "DriveLetter = '$UserVar_DriveLetter'"
 	if ($check1) {
-		echo "- Error: There is already a disk mounted to `"${UserVar_DriveLetter}`" - Operation Aborted!"
+		"- Error: There is already a disk mounted to `"${UserVar_DriveLetter}`" - Operation Aborted!"
 		return
 	} 
 	# Check to make sure this isn't the Windows system drive.
@@ -20,28 +20,28 @@ function Set-DriveLetter($DriveLabel,$DriveLetter) {
 	$check2a = $check2.DriveLetter
 	$check2b = Test-Path $check2a\Windows
 	if ($check2b -eq $True) {
-		echo "- Error: $check2a is a System Disk - Operation Aborted!"
+		"- Error: $check2a is a System Disk - Operation Aborted!"
 		return
 	}
 
 	$drive = Get-WmiObject -Class win32_volume -Filter "label = '$DriveLabel'"
 	if ($drive) {
-		echo "- Found: Assigned drive letter `"$DriveLetter`" to volume labeled `"$DriveLabel`""
+		"- Found: Assigned drive letter `"$DriveLetter`" to volume labeled `"$DriveLabel`""
 		$drive.DriveLetter = $DriveLetter
 		$drive.Put() >$null
 	} 
 	else {
-		echo "- Not found: Skipping"
+		"- Not found: Skipping"
 	}
 }
 
 # Script
-"-"*80 | echo
-echo "Desired Drive Letter : $UserVar_DriveLetter"
-echo "Volume Label Prefix  : $UserVar_LabelPrefix"
-echo "Drives in Rotation   : $UserVar_TotalDrives"
-"-"*80 | echo
-echo ""
+"-"*80
+"Desired Drive Letter : $UserVar_DriveLetter"
+"Volume Label Prefix  : $UserVar_LabelPrefix"
+"Drives in Rotation   : $UserVar_TotalDrives"
+"-"*80
+""
 
 for ($i=1; $i -le $UserVar_TotalDrives; $i++) {
 	if ($i -lt 10) {
@@ -52,7 +52,7 @@ for ($i=1; $i -le $UserVar_TotalDrives; $i++) {
 	}
 	echo "Looking for disk $i ($DriveLabel)"
 	Set-DriveLetter -DriveLabel $DriveLabel -DriveLetter $UserVar_DriveLetter
-	echo ""
+	""
 }
 
-"-"*80 | echo
+"-"*80
