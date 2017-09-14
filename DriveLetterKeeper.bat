b09ee48e48c6ea523f2ce8b7c5723eb1ceab19cb
Allison <acreely@outlook.com>
2017-09-14 11:50:25 -0700

    Original upload
    Launches PowerShell script in the required environment (for testing).

---
+++ DriveLetterKeeper.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+:: PowerShell Script Launcher (with permissions check). Requires elevation.
+
+SET TestFile=%windir%\AdminCheck%RANDOM%.txt
+ECHO Checking write permissions > %TestFile%
+IF NOT EXIST %TestFile% (
+	EXIT
+)
+DEL /F %TestFile%
+
+CD %~dp0
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1'"
+ECHO.
+PAUSE
+EXIT
