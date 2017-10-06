b45a6e46d33df42c6b6b6b2396b181412c575a7c
Allison <acreely@outlook.com>
2017-10-05 20:55:50 -0700

    Update DriveLetterKeeper.ps1
    Switched to an alternative (more compatible) method for changing drive letter.

--- DriveLetterKeeper.ps1
+++ DriveLetterKeeper.ps1
@@ -7,27 +7,28 @@ $UserVar_DriveLetter = "v:"
 $UserVar_LabelPrefix = "Backup"
 $UserVar_TotalDrives = "2"
 $UserVar_EraseNewDrv = "True"
+$UserVar_MakeRootDir = "VeeamBackup"
 $UserVar_SkipPrompts = "False"
 
 # Functions
-function Set-DriveLetter($DriveLabel,$DriveLetter,$DeleteContents) {
+function Set-DriveLetter($DriveLabel,$DriveLetter,$DeleteContents,$RootDirectory) {
 	# Collect volume information
 	$drive = Get-WmiObject -Class win32_volume -Filter "label = '$DriveLabel'"
 	$check = Get-WmiObject -Class win32_volume -Filter "DriveLetter = '$DriveLetter'"
 	
 	#Check if label exists
 	if (!$drive) {
-		Write-Output "- Not found: Skipping"
+		Write-Output "- Not found : Skipping"
 		return
 	}
 	# Check to make sure there isn't a disk mounted to the destination drive letter
 	if ($check) {
 		$FoundLabel = $check.label
 		if ($drive.DriveLetter -eq $DriveLetter) {
-			Write-Output "- Found: Drive letter `"$DriveLetter`" already assigned to volume labeled `"$FoundLabel`""
+			Write-Output "- Found : Drive letter `"$DriveLetter`" already assigned to volume labeled `"$FoundLabel`""
 		}
 		else {
-			Write-Output "- Error: Volume `"$FoundLabel`" already mounted to `"$DriveLetter`" - Operation Aborted!"
+			Write-Output "- Error : Volume `"$FoundLabel`" already mounted to `"$DriveLetter`" - Operation Aborted!"
 		}
 		return
 	}
@@ -36,30 +37,37 @@ function Set-DriveLetter($DriveLabel,$DriveLetter,$DeleteContents) {
 		$check2a = $drive.DriveLetter
 		$check2b = Test-Path $check2a\Windows
 		if ($check2b -eq $True) {
-			Write-Output "- Error: $check2a is a System Disk - Operation Aborted!"
+			Write-Output "- Error : $check2a is a System Disk - Operation Aborted!"
 			return
 		}
 	}
 	# Change Drive Letter
 	if ($drive) {
-		Write-Output "- Found: Assigning drive letter `"$DriveLetter`" to volume labeled `"$DriveLabel`""
-		$drive.DriveLetter = $DriveLetter
-		$drive.Put() >$null
+		Write-Output "- Found : Assigning drive letter `"$DriveLetter`" to volume labeled `"$DriveLabel`""	
+		$DriveOld = $drive.DriveLetter.Trim(":")
+		$DriveNew = $DriveLetter.Trim(":")
+		"select volume " + $DriveOld + [char]13 + [char]10 + "assign letter " + $DriveNew | diskpart > $Null
 		
 		# Verify Operation
 		$Verify_DriveInfo   = Get-WmiObject -Class win32_volume -Filter "label = '$DriveLabel'"
 		$Verify_DriveLetter = $Verify_DriveInfo.DriveLetter
 		if ($Verify_DriveLetter -eq $DriveLetter) {
-			Write-Output "- Info:  Drive letter was assigned sucessfully"
+			Write-Output "- Info  : Drive letter was assigned sucessfully"
 			
 			# Delete contents of disk, if requested
 			if ($DeleteContents -eq "True") {
-				Write-Output "- Info:  Deleting the contents of `"$DriveLetter`""
+				Write-Output "- Info  : Deleting contents of `"$DriveLetter`""
 				Get-ChildItem -Path "$DriveLetter\" -Recurse | Select -ExpandProperty FullName | sort length -Descending | Remove-Item -force >$null
+				
+				# Create root directory, if requested
+				if ($RootDirectory) {
+					Write-Output "- Info  : Creating directory `"$RootDirectory`" on `"$DriveLetter`""
+					New-Item "$DriveLetter\$RootDirectory" -type directory >$null
+				}
 			}
 		}
 		else {
-			Write-Output "- Error: Unable to assign drive letter - Operation Aborted!"
+			Write-Output "- Error : Unable to assign drive letter - Operation Aborted!"
 		}
 	}
 }
@@ -98,7 +106,7 @@ for ($i=1; $i -le $UserVar_TotalDrives; $i++) {
 		$DriveLabel = "${UserVar_LabelPrefix}$i"
 	}
 	Write-Output "Looking for disk $i ($DriveLabel)"
-	Set-DriveLetter -DriveLabel $DriveLabel -DriveLetter $UserVar_DriveLetter -DeleteContents $UserVar_EraseNewDrv
+	Set-DriveLetter -DriveLabel $DriveLabel -DriveLetter $UserVar_DriveLetter -DeleteContents $UserVar_EraseNewDrv -RootDirectory $UserVar_MakeRootDir
 	Write-Output ""
 }
 
